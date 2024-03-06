8fdc92bc38e1ed5b2bdeb9d2585bdbd1cedde067
aarostegui <andoni.arostegui@stacknvault.com>
2024-03-06 15:41:01 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,60 +2,60 @@
   import QrCode from "../components/QrCode.svelte";
   import moment from "moment";
   export let data;
-  let tickets = [
-    {
-      eventId: 15701,
-      img: 14999,
-      eventStart: 1722456000,
-      eventEnd: 1722469500,
-      eventName: "EVENTO PRUEBA DEL NUEVO NOMBRE",
-      qrCode: "8c94da999fbd445baa215f8ea650c89f",
-      customer: {
-        name: "Internal",
-        phone: "1234",
-        email: "abcd@foo.com",
-        address: "Internal address",
-      },
-      venue: "Palacio Blanco",
-      seat: "AZUL-VIP-SECC-C3-ASTO-JJ18",
-      ticketId: 16411,
-    },
-    {
-      eventId: 15701,
-      img: 14999,
-      eventStart: 1722456000,
-      eventEnd: 1722469500,
-      eventName: "EVENTO PRUEBA DEL NUEVO NOMBRE",
-      qrCode: "5586a87843304ff890cfefb6c86c6609",
-      customer: {
-        name: "Internal",
-        phone: "1234",
-        email: "abcd@foo.com",
-        address: "Internal address",
-      },
-      venue: "Palacio Blanco",
-      seat: "DIAMANTE-SECC-A3-ASTO-N15",
-      ticketId: 16412,
-    },
-    {
-      eventId: 15701,
-      img: 14999,
-      eventStart: 1722456000,
-      eventEnd: 1722469500,
-      eventName: "EVENTO PRUEBA DEL NUEVO NOMBRE",
-      qrCode: "1218567199ef49978da3abe8aa0b9383",
-      customer: {
-        name: "Internal",
-        phone: "1234",
-        email: "abcd@foo.com",
-        address: "Internal address",
-      },
-      venue: "Palacio Blanco",
-      seat: "PLATINUM-SECC-B3-ASTO-U13",
-      ticketId: 16413,
-    },
-  ];
-  // let tickets = [];
+//   let tickets = [
+//     {
+//       eventId: 15701,
+//       img: 14999,
+//       eventStart: 1722456000,
+//       eventEnd: 1722469500,
+//       eventName: "EVENTO PRUEBA DEL NUEVO NOMBRE",
+//       qrCode: "8c94da999fbd445baa215f8ea650c89f",
+//       customer: {
+//         name: "Internal",
+//         phone: "1234",
+//         email: "abcd@foo.com",
+//         address: "Internal address",
+//       },
+//       venue: "Palacio Blanco",
+//       seat: "AZUL-VIP-SECC-C3-ASTO-JJ18",
+//       ticketId: 16411,
+//     },
+//     {
+//       eventId: 15701,
+//       img: 14999,
+//       eventStart: 1722456000,
+//       eventEnd: 1722469500,
+//       eventName: "EVENTO PRUEBA DEL NUEVO NOMBRE",
+//       qrCode: "5586a87843304ff890cfefb6c86c6609",
+//       customer: {
+//         name: "Internal",
+//         phone: "1234",
+//         email: "abcd@foo.com",
+//         address: "Internal address",
+//       },
+//       venue: "Palacio Blanco",
+//       seat: "DIAMANTE-SECC-A3-ASTO-N15",
+//       ticketId: 16412,
+//     },
+//     {
+//       eventId: 15701,
+//       img: 14999,
+//       eventStart: 1722456000,
+//       eventEnd: 1722469500,
+//       eventName: "EVENTO PRUEBA DEL NUEVO NOMBRE",
+//       qrCode: "1218567199ef49978da3abe8aa0b9383",
+//       customer: {
+//         name: "Internal",
+//         phone: "1234",
+//         email: "abcd@foo.com",
+//         address: "Internal address",
+//       },
+//       venue: "Palacio Blanco",
+//       seat: "PLATINUM-SECC-B3-ASTO-U13",
+//       ticketId: 16413,
+//     },
+//   ];
+  let tickets = [];
   console.log(data);
   let busy = false;
   const createTickets = async () => {
