ba06efe428c1adffcc5b7c3b10de4637f26e23f4
aarostegui <andoni.arostegui@stacknvault.com>
2024-03-06 15:36:55 +0100

    Env vars for tickets

---
+++ src/lib/server/constants.ts
@@ -0,0 +1,38 @@
+export const eventData = {
+    production: {
+        seatListMetaId: 127892,
+        ticket: {
+            eventId: 15090,
+            bookingId: 18501,
+            img: 16367,
+            eventStart: 1710016200,
+            eventEnd: 1710027900,
+            eventName: 'Kurt en Concierto',
+            customer: {
+                name: 'Internal',
+                phone: '1234',
+                email: 'abcd@foo.com',
+                address: 'Internal address',
+            },
+            venue: 'Teatro Metropolitano',
+        }
+    },
+    local: {
+        seatListMetaId: 98976,
+        ticket: {
+            eventId: 15701,
+            bookingId: 16388,
+            img: 14999,
+            eventStart: 1722456000,
+            eventEnd: 1722469500,
+            eventName: 'EVENTO PRUEBA DEL NUEVO NOMBRE',
+            customer: {
+                name: 'Internal',
+                phone: '1234',
+                email: 'abcd@foo.com',
+                address: 'Internal address',
+            },
+            venue: 'Palacio Blanco',
+        }
+    }
+}
